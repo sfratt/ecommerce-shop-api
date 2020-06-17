f9c6b7d3ed32be80b083730293a2d0d39bc68a76
sfratt <sfratt@users.noreply.github.com>
2020-06-17 15:17:15 -0400

    Enable body parsing and CORS error handling

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,25 +8,30 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
+    const order = {
+        productId: req.body.productId,
+        quantity: req.body.quantity
+    };
     res.status(201).json({
-        message: "Orders were created"
+        message: "Orders were created",
+        order: order
     });
 });
 
 router.get("/:orderId", (req, res, next) => {
     const id = req.params.orderId;
     res.status(200).json({
-            message: "Order details",
-            id: id
-        });
+        message: "Order details",
+        id: id
+    });
 });
 
 router.delete("/:orderId", (req, res, next) => {
     const id = req.params.orderId;
     res.status(200).json({
-            message: "Order deleted",
-            id: id
-        });
+        message: "Order deleted",
+        id: id
+    });
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require("express");
+const logger = require("morgan");
+
+const productsRouter = require("./api/routes/products");
+const ordersRouter = require("./api/routes/orders");
+
 const app = express();
-const morgan = require("morgan");
 
-const productRoutes = require("./api/routes/products");
-const orderRoutes = require("./api/routes/orders");
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(morgan("dev"));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
 
-app.use("/products", productRoutes);
-app.use("/orders", orderRoutes);
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.use("/products", productsRouter);
+app.use("/orders", ordersRouter);
 
 app.use((req, res, next) => {
     const error = new Error("Not found");
